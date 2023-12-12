ac6ebacec0e899d2d4d337d1b198fe8fe6f412a9
01adrien <adriengaucher02@gmail.com>
2023-12-12 22:37:10 +0100

    12/12/23 PRM

--- PRM/java_tp/tp_8/Seq/src/Seq.java
+++ PRM/java_tp/tp_8/Seq/src/Seq.java
@@ -92,12 +92,126 @@ public class Seq {
         return err ? false : true;
     }
 
+    public static boolean has_step(SeqInt seq) {
+        SeqIntIterator iter = seq.iterator();
+        boolean res = false;
+        if (!isEmpty(seq)) {
+            int current = iter.next();
+            while (iter.hasNext()) {
+                int next = iter.next();
+                if (next == current) {
+                    res = true;
+                }
+                current = next;
+            }
+        }
+        return res;
+    }
+
+    public static int last_pos_max(SeqInt seq) {
+        SeqIntIterator iter = seq.iterator();
+        int pos = -1;
+        int current;
+        if (!isEmpty(seq)) {
+            int m = max(seq);
+            for (int i = 0; iter.hasNext(); i++) {
+                current = iter.next();
+                if (current == m) {
+                    pos = i;
+                }
+            }
+
+        }
+        return pos;
+    }
+
+    public static int first_pos_max(SeqInt seq) {
+        SeqIntIterator iter = seq.iterator();
+        int pos = -1;
+        int current;
+        boolean found = false;
+        if (!isEmpty(seq)) {
+            int m = max(seq);
+            for (int i = 0; iter.hasNext() && !found; i++) {
+                current = iter.next();
+                if (current == m) {
+                    pos = i;
+                    found = true;
+                }
+            }
+
+        }
+        return pos;
+    }
+
+    public static int count_max(SeqInt seq) {
+        SeqIntIterator iter = seq.iterator();
+        int count = 0;
+        int current;
+        if (!isEmpty(seq)) {
+            int m = max(seq);
+            while (iter.hasNext()) {
+                current = iter.next();
+                if (current == m) {
+                    count++;
+                }
+            }
+
+        }
+        return count;
+    }
+
+    public static int max_neg(SeqInt seq) {
+        SeqIntIterator iter = seq.iterator();
+        int m = 0;
+        if (!isEmpty(seq)) {
+            while (iter.hasNext()) {
+                int current = iter.next();
+                if (current < 0) {
+                    if (m == 0) {
+                        m = current;
+                    } else if (current > m) {
+                        m = current;
+                    }
+                }
+            }
+        }
+        return m;
+
+    }
+
+    public static SeqInt min_max(SeqInt seq) {
+        int min = 0;
+        int max = 0;
+        int current;
+        SeqIntIterator iter = seq.iterator();
+        if (!isEmpty(seq)) {
+            max = min = iter.next();
+            while (iter.hasNext()) {
+                current = iter.next();
+                if (current > max) {
+                    max = current;
+                }
+                if (current < min) {
+                    min = current;
+                }
+            }
+        }
+
+        return new SeqInt(max, min);
+    }
+
     public static void main(String[] args) throws Exception {
-        SeqInt seq = new SeqInt(1, 2, 3, 4, 8, 9);
+        SeqInt seq = new SeqInt(1, 0);
         System.out.println();
         try {
-            System.out.println(is_asc(seq) ? "OK" : "PAS OK");
+            SeqInt mn = min_max(seq);
+            SeqIntIterator iter = mn.iterator();
+            System.out.println("MAX => " + iter.next());
+            System.out.println("MIN => " + iter.next());
+            System.out.println();
         } catch (Exception e) {
+            System.out.println("Catch something");
             System.out.println(e.toString());
         }
         System.out.println();
