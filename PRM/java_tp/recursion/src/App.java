2d60c50c1f65e77a83d457fec831494207b84578
Adrien GAUCHER <0811adgaucher@student.epfc.eu>
2024-04-09 20:45:52 +0200

    PRM

--- PRM/java_tp/recursion/src/App.java
+++ PRM/java_tp/recursion/src/App.java
@@ -12,12 +12,16 @@ public class App {
 
     // EX 2
     public static int fact(int n) {
-        return n > 1 ? n * fact(n - 1) : n;
+        return n > 1
+                ? n * fact(n - 1)
+                : n;
     }
 
     // EX 3
     public static int fib(int n) {
-        return n >= 2 ? fib(n - 2) + fib(n - 1) : n;
+        return n >= 2
+                ? fib(n - 2) + fib(n - 1)
+                : n;
     }
 
     // EX 4
@@ -32,17 +36,23 @@ public class App {
 
     // EX 5
     public static int pow2(int n, int exp) {
-        return isEven(n) ? pow(n * n, exp / 2) : pow(n * n, (exp - 1) / 2) * n;
+        return isEven(exp)
+                ? pow(n * n, exp / 2)
+                : pow(n * n, (exp - 1) / 2) * n;
     }
 
     // EX 6
     public static int ex6(int n) {
-        return n >= 10 ? 1 + ex6(n / 10) : 1;
+        return n >= 10
+                ? 1 + ex6(n / 10)
+                : 1;
     }
 
     // EX 7
     public static int ex7(int n) {
-        return n < 10 ? n : n % 10 + ex7(n / 10);
+        return n < 10
+                ? n
+                : n % 10 + ex7(n / 10);
     }
 
     // EX 8
@@ -73,18 +83,79 @@ public class App {
 
     // EX 10
     public static int ex10(int n) {
-        return n <= 1 ? 0 : 1 + ex10(n - 2);
+        return n <= 1
+                ? 0
+                : 1 + ex10(n - 2);
     }
 
     // EX 11
     public static boolean ex11(int n) {
-        return n < 10 ? isEven(n) : isEven(n % 10) && ex11(n / 10);
+        return n < 10
+                ? isEven(n)
+                : isEven(n % 10) && ex11(n / 10);
+    }
+
+    // EX 12
+
+    public static void ex12(int n) {
+        System.out.print(n % 10);
+        if (n > 9) {
+            ex12(n / 10);
+        }
+    }
+
+    // EX 13
+
+    public static int ex13(int n) {
+        return n < 10
+                ? n
+                : Math.max(n % 10, ex13(n / 10));
+    }
+
+    // EX 14
+    public static int ex14(int n, int exp) {
+        if (n == 1) {
+            return pow2(2, exp);
+        } else if (n == 0) {
+            return 0;
+        } else if (n % 10 == 1) {
+            return pow2(2, exp) + ex14(n / 10, exp + 1);
+        } else {
+            return ex14(n / 10, exp + 1);
+        }
+    }
+
+    // EX 15
+    public static int ex15(Array<Integer> arr, int pos) {
+        return (pos == arr.size() - 1)
+                ? arr.get(pos)
+                : Math.max(arr.get(pos), ex15(arr, pos + 1));
+    }
+
+    public static int ex15Bis(Array<Integer> arr, int low, int high) {
+        int mid = (high + low) / 2;
+        return low == high
+                ? arr.get(high)
+                : Math.max(ex15Bis(arr, low, mid), ex15Bis(arr, mid + 1, high));
+    }
+
+    public static int isCap(char c) {
+        return c >= 'A' && c <= 'Z'
+                ? 1
+                : 0;
+    }
+
+    // EX 16
+    public static int ex16(String str) {
+        return str.length() == 0
+                ? 0
+                : isCap(str.charAt(0)) + ex16(str.substring(1));
     }
 
     public static void main(String[] args) throws Exception {
-        Array<Integer> arr = new Array<>(1, 5, 8, 15, 19, 26, 31, 39);
+        Array<Integer> arr = new Array<>(1, 5, 8, 15, 99, 19, 26, 31, 39);
         // System.out.println(binSearch(arr, 0, arr.size() - 1, 89));
-        System.out.println(ex11(2864882));
+        System.out.println(ex14(10100110, 0));
     }
 
 }
