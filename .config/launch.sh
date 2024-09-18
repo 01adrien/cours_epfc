0c7e3e5938da2194bd4d104ba728838e7277cd63
01adrien <adriengaucher02@gmail.com>
2024-09-18 16:41:20 +0200

    clean OK

--- .config/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/adrien/Bureau/cours_epfc
-git pull
-cat .config/.bashrc > ~/.bashrc
-cat .config/VScode/settings.json > ~/.config/Code/User/settings.json
-cat .config/VScode/keybindings.json > ~/.config/Code/User/keybindings.json
