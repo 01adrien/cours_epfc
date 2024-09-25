b6a7a71532afbbb03e2193329efc13e59bce5124
01adrien <adriengaucher02@gmail.com>
2024-09-25 10:01:46 +0200

    install PHP

--- 2eme_annee/POO/date/src/Test.java
+++ 2eme_annee/POO/date/src/Test.java
@@ -60,5 +60,6 @@ public class Test {
         //testDayOfWeek();
         //testPersonn();
         testCreateIllegalDate();
+        System.out.println("All test passed.");
     }
 }
--- 2eme_annee/WEB/PHP_base/ex5.php
+++ 2eme_annee/WEB/PHP_base/ex5.php
@@ -1,65 +1,67 @@
 <pre>
 <?php
 
-$min = false;
-$max = false;
-$max_num;
-include './check_values.php';
-
-if(isset($_GET["max_num"])) {
-    $max_num = $_GET["max_num"];
-} else {
-    $max_num = $max - $min;
-}
-
-
-if (isset($_GET["min"])) {
-    $min = (int)$_GET["min"];
-    $max = (int)$_GET["max"];
-} elseif (isset($_POST["min"])) {
-    $min = (int)$_POST["min"];
-    $max = (int)$_POST["max"];
-}
-
-
-if ($min > $max || $min < 0 || $max < 0) {
-    echo "<p>invalid integer value for min or max</p>";
-    exit(3);
-}
-
-
-// PAGINATION
-$max_per_pages = 20;
-$page_number = $max_per_pages / $max_per_pages;
-$need_pagination = $page_number > 1;
-$max_page = $max_num / $max_per_pages;
-
-
-
-if ($need_pagination) {
-    $nmin *= $page_number;
-    $nmax = $min + $max_per_pages;
-    if ($page_number == $max_page) {
-        $nmax = $max_num - $nmin;
-    }
-} else {
-    $nmax = $max;
-    $nmin = $min;
-}
-
-?>
-<ul>
-<?php
-if ($max > 0) {   
-    for ($i=$nmin; $i <= $nmax ; $i++) { 
-        echo "<li>$i</li>";
+function check_and_set($array, $key, $var): void
+{
+    if (isset($array[$key])) {
+        $param = $array[$key];
+        if (is_numeric($param)) {
+            $var = $param;
+        } else {
+            echo "<p>min and max must be numeric value</p>";
+            exit(1);
+        }
     }
 }
 
-//if ($need_pagination) {
-    include_once 'pagination.php';
-//}
+function main(): void
+{
+
+    $min = false;
+    $max = false;
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        check_and_set($_POST, "min", $min);
+        check_and_set($_POST, "max", $max);
+    } else {
+        check_and_set($_GET, "max", $max);
+        check_and_set($_GET, "min", $min);
+    }
+    if ($min === false || $max === false) {
+        echo "<p>min or max are not set</p>";
+        exit(1);
+    }
+
+    $max_per_pages = 20;
+    $page_number = $max_per_pages / $max_per_pages;
+    $need_pagination = $page_number > 1;
+    $max_page = $max / $max_per_pages;
+    $current_page = isset($_GET["page"]) ? intval($_GET["page"]) : 1;
+
+    if ($need_pagination) {
+
+    }
+
+    echo "<ul>";
+    if ($max > 0) {
+        for ($i = $min; $i <= $max; $i++) {
+            echo "<li>$i</li>";
+        }
+    }
+
+    if ($need_pagination) {
+        if ($current_page > 1) {
+            echo "<a href='./ex6.php?min=$min&max=$max&page=$current_page'>prev</a>";
+        }
+        echo "<a href='./ex6.php?min=$min&max=$max&page=$current_page'>next</a>";
+    }
+
+    echo "</ul>";
+
+}
+
+main();
+
 
 ?>
-</ul>
-<pre>
+</pre>
