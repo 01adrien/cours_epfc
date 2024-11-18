da7f6ff34be4c2225ab13c0aca0b1265add2e7b2
Adrien GAUCHER <0811adgaucher@student.epfc.eu>
2024-11-18 21:25:55 +0100

    POO

---
+++ 2eme_annee/POO/sources12/sources12/src/main/java/eu/epfc/labo12/decoratedmicrowave/DecoratedMicrowave.java
@@ -0,0 +1,30 @@
+package eu.epfc.labo12.decoratedmicrowave;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class DecoratedMicrowave extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
